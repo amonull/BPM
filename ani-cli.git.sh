813e3ed806f834334063eb4a87c3eb99cf2ad0fc
amonull <90932550+amonull@users.noreply.github.com>
2022-12-08 19:09:07 +0000

    added more comments
    more comments added to explain functions/variables and their uses and how to use them

--- ani-cli.git.sh
+++ ani-cli.git.sh
@@ -2,6 +2,7 @@
 
 USER_WIDE_INSTALLATION=false
 
+# dependencies will target certain package managers make sure to include dependencies for your package manager
 # VOID
 DEPENDENCIES_XBPS="grep sed awk curl openssl mpv axel ffmpeg"
 OPTIONAL_DEPENDENCIES_XBPS="fzf"
@@ -26,14 +27,22 @@ OPTIONAL_DEPENDENCIES_ZYPPER="fzf"
 DEPENDENCIES_PORTAGE="grep sed awk curl openssl mpv axel ffmpeg"
 OPTIONAL_DEPENDENCIES_PORTAGE="fzf"
 
+# download method must have either "git clone", "curl", or "wget" to start with and can have more options defined inside later
+# ie: 
+# DOWNLOAD_METHOD="git clone --quite" == valid 
+# DOWNLOAD_METHOD="--quite git clone" == invalid (invalid for both BPM and git)
 DOWNLOAD_METHOD="git clone"
 
 URL="https://github.com/pystardust/ani-cli.git"
 
+# if function is not begin used add:
+# echo "TODO:"; exit 1
 install_cmd () {
     mv /tmp/BPM.*/ani-cli $HOME/.local/BPM/pkgs/
 }
 
+# if function is not begin used add:
+# echo "TODO:"; exit 1
 remove_cmd () {
     rm -f $HOME/.local/BPM/pkgs/ani-cli
 }
