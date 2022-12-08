5429a34cae430c0a26f932c84a4c4dce48909d7a
amonull <90932550+amonull@users.noreply.github.com>
2022-12-08 14:33:50 +0000

    added example file

---
+++ ani-cli.git.sh
@@ -0,0 +1,37 @@
+USER_WIDE_INSTALLATION=false
+
+# VOID
+DEPENDENCIES_XBPS="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_XBPS="fzf"
+
+# DEBIAN
+DEPENDENCIES_DPKG="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_DPKG="fzf"
+
+# ARCH
+DEPENDENCIES_PACMAN="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_PACMAN="fzf"
+
+# FEDORA
+DEPENDENCIES_RPM="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_RPM="fzf"
+
+# SUSE
+DEPENDENCIES_ZYPPER="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_ZYPPER="fzf"
+
+# GENTOO
+DEPENDENCIES_PORTAGE="grep sed awk curl openssl mpv axel ffmpeg"
+OPTIONAL_DEPENDENCIES_PORTAGE="fzf"
+
+DOWNLOAD_METHOD="git clone"
+
+URL="https://github.com/pystardust/ani-cli.git"
+
+install_cmd () {
+    mv /tmp/BPM.*/ani-cli $HOME/.local/BPM/pkgs/
+}
+
+remove_cmd () {
+    rm -f $HOME/.local/BPM/pkgs/ani-cli
+}
